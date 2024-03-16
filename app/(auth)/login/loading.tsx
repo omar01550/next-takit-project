be92b5c226da826dfd34bc83e699ff03b43675b1
omar01550 <arom57818@gmail.com>
2024-03-16 16:55:49 +0200

    add loading state for login

---
+++ app/(auth)/login/loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading  = () => {
+  return (
+    
+    <main className="w-full min-h-screen flex justify-center items-center">
+            <h1>loading ................</h1>
+    </main>
+  )
+}
+
+export default Loading 
