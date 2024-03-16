c7bc8bd6e2c4f1d90d20ea301f8a7705ab65392f
omar01550 <arom57818@gmail.com>
2024-03-16 17:44:33 +0200

    optmize login form

--- app/(auth)/login/loginForm.tsx
+++ app/(auth)/login/loginForm.tsx
@@ -10,120 +10,126 @@ import {useForm} from 'react-hook-form';
 import { z } from 'zod';
 import { ArrowLeft, ArrowRight } from 'lucide-react';
 import ArrowRightAltOutlinedIcon from '@mui/icons-material/ArrowRightAltOutlined';
-import { log } from 'console';
 import { useRouter } from 'next/navigation';
-
 
 
 
 
 const LoginForm = () => {
 
- const route = useRouter();
-
+  const route = useRouter();
   const [showPassword,setShowPassword]  = useState(false);
+  const {register,handleSubmit,formState,getValues,setError} = useForm()
+  const [loading,setLoading] = useState(false);
 
-  const {register,handleSubmit,formState,getValues} = useForm({
       
-  });
-   
+  
+  //handle  Login
+  
 
-
-  return (
+const  handleLogin =(data) => {
+  setLoading(true)
+  if (data) {
+   fetch("http://localhost:3000/api/login",{
+      method:"POST",
+      body:JSON.stringify(data)
+   }).then((res) => {
+      if(res.status == 200){
+       return res.json()
+      }else{
+        return new Promise((res,rej)=>{rej("rej")})
+      }
+   })
+   .then((result) => {
     
-   <form action="" className="flex flex-col items-center space-y-6 mt-12 w-full  " onSubmit={handleSubmit((data) => {
-         if (data) {
-          fetch("https://next-takit-project.vercel.app/api/login",{
-             method:"POST",
-             body:JSON.stringify(data)
-          }).then((res) => {
-             if(res.status == 200){
-              return res.json()
-             }else{
-               return new Promise((res,rej)=>{rej("rej")})
-             }
-          })
-          .then((result) => {
 const token = result.token ;
 document.cookie=`token=${token}`;
 if (token ) {
-    route.push("/")    
+route.push("/")    
 }
-            
-          })
-          .catch((error) => {
-            console.log('error');
-            
-          })
-          
-         }   
-      
-    })}>
-
-{/* email field */}
-  <p className="text-red-500 error-message text-start text-bold text-xl">
-        {
-           formState.errors.email&& <span>{formState.errors.email.message}</span>
-        }
-    </p>  
-
-    <div className="input-container bg-gray-100 flex justify-start items-center rounded-md p-4 space-x-2 w-full md:w-[380px]">
-    <MailOutlineIcon  />
-          <input type="email" placeholder='enter your email' className="bg-gray-100 text-blue-900 text-2xl focus:outline-none w-10/12"
-            {...register("email", {
-            required:{value:true,message:"email is required"},
-             pattern: {
-               value:/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-               message:"in valied email",
-
-             }
-            })}
-          />
-          
-    </div>
-
-{/* email field */}
-  
-
-  {/* password field */}
+     
+   })
+   .catch((error) => {
+       setError('email',{message:"invalid email or password"})
+     
+   }).finally(() => {
+    setLoading(false)
+   })
    
+  }   
 
-<p className="text-red-500 error-message text-start text-bold text-xl">
-        {
-           formState.errors.password&& <span>
-             {formState.errors.password.message}
-           </span>
-        }
-    </p>
-    <div className="input-container bg-gray-100 flex justify-start items-center rounded-md p-4 space-x-2 w-full md:w-[380px]">
-         <HttpsOutlinedIcon/>
-         <input type={showPassword?"text":"password"} placeholder='enter your password' className="bg-gray-100 text-blue-900 text-2xl focus:outline-none w-10/12"
-             {...register("password")}
-         />
-          <span onClick={() => {
-             setShowPassword(!showPassword)
-          }} className='cursor-pointer'>
-          {
-             showPassword?<VisibilityOffIcon/>:<VisibilityIcon/>
-          }
-          </span>
-    </div>
+};
 
-  {/* password field */}
-  <Link href={'/forgotpassword'} className='text-end text-omar-100 underline w-full font-semibold text-base md:text-xl capitalize flex justify-end items-center'>
-         forgot your password ?
-    </Link>
+    
 
-    <Link href={'/signup'} className='text-end text-omar-100 underline w-full font-semibold text-base md:text-xl capitalize flex justify-end items-center'>
-         create new account<ArrowRightAltOutlinedIcon/>
-    </Link>
+  return (
+    
+ <form action="" className="flex flex-col items-center space-y-6 mt-12 w-full" onSubmit={handleSubmit(handleLogin)}>
+
+      {/* email field */}
+        <p className="text-red-500 error-message text-start text-bold text-xl">
+              {
+                formState.errors.email&& <span>{formState.errors.email.message}</span>
+              }
+          </p>  
+
+          <div className="input-container bg-gray-100 flex justify-start items-center rounded-md p-4 space-x-2 w-full md:w-[380px]">
+              <MailOutlineIcon  />
+                <input type="email" placeholder='enter your email' className="bg-gray-100 text-blue-900 text-2xl focus:outline-none w-10/12"
+                  {...register("email", {
+                  required:{value:true,message:"email is required"},
+                  pattern: {
+                    value:/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+                    message:"in valied email",
+
+                  }
+                  })}
+                />
+                
+          </div>
+
+      {/* email field */}
+        
+
+        {/* password field */}
+        
+
+      <p className="text-red-500 error-message text-start text-bold text-xl">
+              {
+                formState.errors.password&& <span>
+                  {formState.errors.password.message}
+                </span>
+              }
+          </p>
+          <div className="input-container bg-gray-100 flex justify-start items-center rounded-md p-4 space-x-2 w-full md:w-[380px]">
+              <HttpsOutlinedIcon/>
+              <input type={showPassword?"text":"password"} placeholder='enter your password' className="bg-gray-100 text-blue-900 text-2xl focus:outline-none w-10/12"
+                  {...register("password")}
+              />
+                <span onClick={() => {
+                  setShowPassword(!showPassword)
+                }} className='cursor-pointer'>
+                {
+                  showPassword?<VisibilityOffIcon/>:<VisibilityIcon/>
+                }
+                </span>
+          </div>
+
+        {/* password field */}
+        <Link href={'/forgotpassword'} className='text-end text-omar-100 underline w-full font-semibold text-base capitalize flex justify-end items-center md:px-28'>
+              forgot your password ?
+          </Link>
+
+          <Link href={'/signup'} className='text-end text-omar-100 underline w-full font-semibold text-base capitalize flex justify-end items-center md:px-28 '>
+              create new account<ArrowRightAltOutlinedIcon/>
+          </Link>
 
 
-    <Button className='bg-blue-900 rounded-full px-20 py-3 w-fit text-2xl flex justify-center items-center mt-10 hover:bg-blue-800 cursor-pointer'>
-        Login
-    </Button>
+          <Button className='bg-blue-900 rounded-full px-20 py-3 w-fit text-2xl flex justify-center items-center mt-8 hover:bg-blue-800 cursor-pointer'>
+              {loading?<span className='w-8 h-8 rounded-full border-1 border-white border-solid animate-spin'></span>:"Login"}
+          </Button>
 
-</form>
+  </form>
   )
 }
 
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -14,14 +14,14 @@ const LoginPage = () => {
           <ContainerWrapper className='flex w-full'>
            <IntroLogin/>
 
-                <section className="intro w-full lg:w-6/12 bg-myblue py-10 flex flex-col items-center">
-        <div className="form-title">
+    <section className="intro w-full lg:w-6/12 bg-myblue py-10 flex flex-col items-center">
+<div className="form-title">
                 <h2 className="text-3xl lg:text-5xl font-bold text-blue-900 text-center md:text-start">
-                                              welcome to <span className='text-omar-100'>TaK</span>it     
-                                      </h2>
+                                             welcome to <span className='text-omar-100'>TaK</span>it     
+                </h2>
 
                                       <p className="text-omar-100 font-semibold text-base md:text-xl capitalize mt-5 w-full md:w-80 text-center">
-                                        enter your email and  password to reach your account ticket
+                                          enter your email and  password to reach your account ticket
                                       </p>
         </div>
 
