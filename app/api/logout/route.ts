37fdb5bbb42b6cffc4df7acb6f97dd76325ee0a3
omar01550 <155559209+EsmaelMedhat@users.noreply.github.com>
2024-03-28 23:27:04 +0200

    solve logout api issue

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -3,9 +3,5 @@ import { NextResponse } from "next/server"
 
 
 export const GET = async () => {
-    let response = NextResponse.next()
-    // Set a cookie to hide the banner
-    response.cookies.set('token', "")
-    // Response will have a `Set-Cookie:show-banner=false;path=/home` header
-    return response;
+  return NextResponse.json({message:"done"})
 }
