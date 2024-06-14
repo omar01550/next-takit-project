4eb7e96bc52cdac041f60c8b67d702bc3f6c91f5
omar01550 <155559209+EsmaelMedhat@users.noreply.github.com>
2024-06-14 20:58:30 +0300

    optmize home page metadata and seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,15 @@ import Hero from './components/hero';
 import ClientWrapper from '@/context/app';
 import Footer from './components/footer';
 
+export const metadata ={
+  title:"takit home page ",
+  description:`with  Takit: Your one-stop shop for Egypt's rail, bus & metro bookings.
+  Search & compare journeys online.
+  Select your preferred time, service & seat.
+  Fast & secure booking - all from your phone or computer.
+  Experience a simpler way to travel Egypt.`
+
+}
 
 
 
